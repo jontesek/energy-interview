395eb0701f4a9c4045194594b816dd2cc03de3ef
Jonáš Petrovský <jontesek@gmail.com>
2025-07-22 17:50:07 +0200

    feat: add echo_sql to settings.py

--- energy_manager/settings.py
+++ energy_manager/settings.py
@@ -2,6 +2,7 @@ import os
 
 # Database
 DB_CONN = os.getenv("DB_CONN", "sqlite:///energy.db")
+ECHO_SQL = os.getenv("ECHO_SQL", 0)
 # Runtime
 ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
 # Helpers
